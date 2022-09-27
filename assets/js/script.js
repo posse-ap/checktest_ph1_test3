3f2120955d2cfa60a9a0f915cd49b218665b03dd
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-27 16:33:56 +0900

    JavaScript 完成

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,40 @@ const p2 = document.getElementById("panel2");
 const p3 = document.getElementById("panel3");
 
 //各パネルの色変更クラスを読み込み
-const p1c = document.querySelector("service__panel__item__title");
+const c1 = document.getElementById("c1");
+const c2 = document.getElementById("c2");
+const c3 = document.getElementById("c3");
 
-p1c.addEventListener("click", () =>{
-    p1c.style.backgroundColor = "lightblue";
+
+
+//パネルの色、内容変更　記述部分
+c1.addEventListener("click", () =>{
+        //パネルの色
+    c1.style.backgroundColor = "#333";
+    c2.style.backgroundColor = "#a5a5a5";
+    c3.style.backgroundColor = "#a5a5a5";
+    //記述内容
+    p1.style.display = "block";
+    p2.style.display = "none";
+    p3.style.display = "none";
 });
-
+c2.addEventListener("click", () =>{
+        //パネルの色
+    c2.style.backgroundColor = "#333";
+    c1.style.backgroundColor = "#a5a5a5";
+    c3.style.backgroundColor = "#a5a5a5";
+    //記述内容
+    p2.style.display = "block";
+    p1.style.display = "none";
+    p3.style.display = "none";
+});
+c3.addEventListener("click", () =>{
+        //パネルの色
+    c3.style.backgroundColor = "#333";
+    c1.style.backgroundColor = "#a5a5a5";
+    c2.style.backgroundColor = "#a5a5a5";
+    //記述内容
+    p3.style.display = "block";
+    p1.style.display = "none";
+    p2.style.display = "none";
+});
